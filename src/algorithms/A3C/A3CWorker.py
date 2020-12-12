Fix errors with shareing parameters and gradients

src/algorithms/A3C/A3CWorker.py
@@ -1,9 +1,17 @@
 import torch
 from algorithms.A3C.A3C import A3C
 from utils import ENV_NAME, t
-from algorithms.A3C.actor import Actor
-from algorithms.A3C.critic import Critic
+from algorithms.A3C.Actor import Actor
+from algorithms.A3C.Critic import Critic
 import gym
+
+
+def ensure_shared_grads(model, shared_model):
+    for param, shared_param in zip(model.parameters(),
+                                   shared_model.parameters()):
+        if shared_param.grad is not None:
+            return
+        shared_param._grad = param.grad
 
 
 class A3CWorker:
@@ -13,56 +21,61 @@ class A3CWorker:
         self.env = gym.make(ENV_NAME)
         self.globalA3C = globalA3C
         self.action_space = self.env.action_space
-        self.MAX_EPISODES, self.max_average = globalA3C.MAX_EPISODES, -21.0
+        self.MAX_EPISODES = globalA3C.MAX_EPISODES
         self.lock = globalA3C.lock
-        self.learning_rate = globalA3C.learning_rate
         self.discount_rate = globalA3C.discount_rate
         self.t_max = globalA3C.t_max
         # Instantiate plot memory
-        self.scores, self.episodes, self.average = [], [], []
+        self.scores, self.episodes, self.accum_rewards = [], [], 0
 
         # Create Actor-Critic network model
-        self.Actor = Actor(state_space=self.env.observation_space, learning_rate=self.learning_rate,
+        self.Actor = Actor(global_model_params=globalA3C.Actor.model.parameters(),
+                           state_space=self.env.observation_space, learning_rate=self.globalA3C.actor_learning_rate,
                            action_space=self.action_space)
-        self.Critic = Critic(state_space=self.env.observation_space, learning_rate=self.learning_rate)
+        self.Critic = Critic(global_model_params=globalA3C.Actor.model.parameters(),
+                             state_space=self.env.observation_space, learning_rate=self.globalA3C.critic_learning_rate)
+        self.Actor.model.train()
+        self.Critic.model.train()
 
-    def update_global_models(self, t_start, states, actions, rewards, last_state, last_terminal: bool):
+    def update_global_models(self, states, actions, rewards, last_state, last_terminal: bool):
         R = 0 if last_terminal else self.Critic.predict(last_state)
-        accumulated_reward = 0
-        for i in range(len(states) - 1, t_start - 1, -1):
-            accumulated_reward += rewards[i]
+        self.Actor.optimizer.zero_grad()
+        self.Critic.optimizer.zero_grad()
+        critic_loss = 0
+        actor_loss = 0
+        for i in reversed(range(len(rewards))):
+            self.accum_rewards += rewards[i]
             R = rewards[i] + self.discount_rate * R
             advantage = (R - self.Critic.predict(t(states[i])))
             alpha, beta = self.Actor.predict(t(states[i]))
 
+            torch.distributions.Beta.set_default_validate_args(True)
             dist = torch.distributions.Beta(alpha + 1, beta + 1)
 
-            critic_loss = advantage.pow(2).mean()
-            self.Critic.optimizer.zero_grad()
-            critic_loss.backward()
-            self.Critic.optimizer.step()
+            critic_loss = critic_loss + advantage.pow(2).mean()
+            actor_loss = actor_loss - dist.log_prob(self.Actor.action_to_beta(actions[i])) * advantage.detach()
 
-            actor_loss = -dist.log_prob(actions[i]) * advantage.detach()
-            self.Actor.optimizer.zero_grad()
-            actor_loss.backward()
-            self.Actor.optimizer.step()
-
+        actor_loss.backward()
+        critic_loss.backward()
+        self.globalA3C.Critic.optimizer.step()
+        self.globalA3C.Actor.optimizer.step()
         self.scores.append(R)
-        if self.globalA3C.episode % 200 == 0:
-            print(f"Episode: {self.globalA3C.episode}, accumulated reward for 5 steps: {accumulated_reward}")
+        if self.globalA3C.episode > 0 and self.globalA3C.episode % 100 == 0:
+            print(f"Episode: {self.globalA3C.episode}, accumulated rewards over 100 episodes: {self.accum_rewards}")
+            self.accum_rewards = 0
 
-    def reset_model(self):
+    def sync_models(self):
         self.Actor.set_model_from_global(self.globalA3C.Actor.model)
         self.Critic.set_model_from_global(self.globalA3C.Critic.model)
 
     def run(self):
-        # global graph
         iteration = 1
-        # with graph.as_default():
+        ensure_shared_grads(self.Actor.model, self.globalA3C.Actor.model)
+        ensure_shared_grads(self.Critic.model, self.globalA3C.Critic.model)
         while self.globalA3C.episode < self.MAX_EPISODES:
             is_terminal, saving = False, ''  # Reset gradients etc
             states, actions, rewards = [], [], []  # reset thread memory
-            self.reset_model()
+            self.sync_models()
             t_start = iteration
             state = self.env.reset()  # reset env and get initial state
 
@@ -75,7 +88,7 @@ class A3CWorker:
                 state = next_state
 
             self.lock.acquire()
-            self.update_global_models(t_start, states, actions, rewards, last_state=state, last_terminal=is_terminal)
+            self.update_global_models(states, actions, rewards, last_state=state, last_terminal=is_terminal)
             self.globalA3C.episode += 1
             self.lock.release()
 

src/algorithms/A3C/Actor.py
@@ -5,20 +5,23 @@ from torch import nn
 class Actor:
     train_fn = {}
 
-    def __init__(self, **kwargs):
+    def __init__(self, global_model_params=None, **kwargs):
         self.action_space = kwargs['action_space']
         self.state_space = kwargs['state_space']
         self.learning_rate = kwargs['learning_rate']
         self.model_output_dim = 2  # alpha and beta for beta distribution
         self.model = self.create_model()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        if global_model_params is None:
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        else:
+            self.optimizer = torch.optim.Adam(global_model_params, lr=self.learning_rate)
 
     def create_model(self):
         model = nn.Sequential(
             nn.Linear(self.state_space.shape[0], 64),
-            nn.ReLU(),
+            nn.Sigmoid(),
             nn.Linear(64, 32),
-            nn.ReLU(),
+            nn.Sigmoid(),
             nn.Linear(32, self.model_output_dim),
             nn.ReLU()
         )

src/algorithms/A3C/Critic.py
@@ -3,11 +3,14 @@ from torch import nn
 
 
 class Critic:
-    def __init__(self, **kwargs):
+    def __init__(self, global_model_params=None, **kwargs):
         self.state_space = kwargs['state_space']
         self.learning_rate = kwargs['learning_rate']
         self.model = self.create_model()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        if global_model_params is None:
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        else:
+            self.optimizer = torch.optim.Adam(global_model_params, lr=self.learning_rate)
 
     def create_model(self):
         model = nn.Sequential(